49b27c4294e2a1e73c39d38ed33e7ed8adee8a15
FMBee <f.mevollon@gmail.com>
2017-12-27 19:57:41 +0100

    import réussi

--- ciqual_import.php
+++ ciqual_import.php
@@ -14,17 +14,21 @@
  	foreach( $_LIST_FILES as $file ) {
  		
 	 	$filein  = _CIQUAL_PATH	.str_replace('XXX', $file, _MODEL_FILE);
-	 	$filetmp = _CIQUAL_PATH .'fichier_utf8.xml';
+	 	$filetmp = _CIQUAL_PATH .'fichier_utf8.xml';	// chemin relatif
 	
 	 	$buffer = file_get_contents($filein);
 	 	$buffer = utf8_encode($buffer);
 	 	file_put_contents($filetmp, $buffer);
 	 	
+	 	$filetmp = str_replace('\\', '/', __DIR__ .'/fichier_utf8.xml');	// chemin réel
+	 	
  		$sql = "TRUNCATE {$file};";
  		
- 		$sql .= "LOAD XML local infile '{$filetmp}' into table {$file}"
+ 		$sql .= "LOAD XML infile '{$filetmp}' into table {$file}"
  				." ROWS IDENTIFIED BY '<" .strtoupper($file) .">';"
  		; 
+print_r($sql); 	
+
  		$req = $pdo->prepare($sql);
  		$req->execute();
  	}
--- defines.php
+++ defines.php
@@ -2,11 +2,13 @@
 
 	define('_CIQUAL_PATH', 	'.\\');	//dossier courant
 	define('_MODEL_FILE', 	'XXX_2017 11 21.xml');
+	
 	$_LIST_FILES = array(
 			'alim',
 			'alim_grp',
 			'compo',
 			'const'
+// 			'sources'	// inutile et volumineux
 	);
 
 	define('_DBSERVER',	'mysql:host=localhost;dbname=ciqual2_api');
