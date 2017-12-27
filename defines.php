faf48cbe939a80c62c2f4e06d6cf39403681805e
FMBee <f.mevollon@gmail.com>
2017-12-27 19:03:58 +0100

    first commit

---
+++ defines.php
@@ -0,0 +1,16 @@
+<?php
+
+	define('_CIQUAL_PATH', 	'.\\');	//dossier courant
+	define('_MODEL_FILE', 	'XXX_2017 11 21.xml');
+	$_LIST_FILES = array(
+			'alim',
+			'alim_grp',
+			'compo',
+			'const'
+	);
+
+	define('_DBSERVER',	'mysql:host=localhost;dbname=ciqual2_api');
+	define('_DBUSER',	'root');
+	define('_DBPWD',	'');
+	
+	
